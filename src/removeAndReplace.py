567c82ca0793a49c7ed24fd6fbe8bf64e8401e38
bleierr <bleierr@tcd.ie>
2016-08-03 17:00:16 +0200

    changed to binary strings
    In order to avoid issues with non-standard character binary string are
    used

--- src/removeAndReplace.py
+++ src/removeAndReplace.py
@@ -12,49 +12,56 @@ Roman Bleier
 
 
 def getStopwords(fname):
-    with open(fname, "r") as f:
+    with open(fname, "rb") as f:
         fStrg = f.read()
     stopsLst = []
     for line in fStrg.splitlines():
         stopsLst = stopsLst + [w.lower().strip() for w in line.split()]
+    #print(stopsLst)
     return stopsLst
 
 def replaceFileToTuple(fname):
-    with open(fname, "r") as f:
+    with open(fname, "rb") as f:
         fStrg = f.read()
     replStrgLst = []
     for line in fStrg.splitlines():
-        replStrgLst = replStrgLst + [tuple([strg.strip() for strg in line.split(":")])]
+        replStrgLst = replStrgLst + [tuple([strg.strip() for strg in line.split(b":")])]
     return replStrgLst
 
 
 def removeStopwordsFromFiles(fdir, odir, stops):
     fnames = glob.glob(fdir + os.sep + "*.txt")
     for fname in fnames:
-        with open(fname, "r") as f:
+        with open(fname, "rb") as f:
             fStrg = f.read()
         #Pattern matching possible
 
         notInStops = []
         for line in fStrg.splitlines():
             lineLst = [w.strip() for w in line.strip().split()]
-            
-            for char in [".", ",", ":", ";"]:
-                lineLst = [w.strip(char) for w in lineLst]
 
-            notInStops.append(" ".join([w for w in lineLst if w.lower() not in stops]))
+            
+            #print(lineLst)
+
+            
+            for char in [b".", b",", b":", b";",b"?", b"(", b")"]:
+                lineLst = [w.strip(char) for w in lineLst]
+            notInStops.append(b" ".join([w for w in lineLst if w.lower() not in stops]))
+            #print(stops)
+
+            #print(notInStops)
 
         tail = os.path.split(fname)[1]
 
-        with open(odir+os.sep+tail, "w") as f:
-            f.write("\n".join(notInStops))
+        with open(odir+os.sep+tail, "wb") as f:
+            f.write(b"\n".join(notInStops))
     print("Stopwords removed!")
     return fnames
 
 def replaceStrgInFiles(fdir, odir, replStrgLst):
     fnames = glob.glob(fdir + os.sep + "*.txt")
     for fname in fnames:
-        with open(fname, "r") as f:
+        with open(fname, "rb") as f:
             fStrg = f.read()
 
         replaced = []
@@ -63,10 +70,10 @@ def replaceStrgInFiles(fdir, odir, replStrgLst):
             for replTuple in replStrgLst:
                 #Pattern matching possible with regex, but it has to be used with care
                 replStrg = replTuple[1]
-                if replTuple[0][:2] == "\s": 
-                    replStrg = " " + replStrg
-                if replTuple[0][-2:] == "\s":
-                    replStrg = replStrg + " "
+                if replTuple[0][:2] == b"\s": 
+                    replStrg = b" " + replStrg
+                if replTuple[0][-2:] == b"\s":
+                    replStrg = replStrg + b" "
                 line = replStrg.join(re.split(replTuple[0], line))
                     
                 #line = replTuple[1].join(line.split(replTuple[0]))
@@ -75,8 +82,8 @@ def replaceStrgInFiles(fdir, odir, replStrgLst):
 
         tail = os.path.split(fname)[1]
 
-        with open(odir+os.sep+tail, "w") as f:
-            f.write("\n".join(replaced))
+        with open(odir+os.sep+tail, "wb") as f:
+            f.write(b"\n".join(replaced))
     print("Pattern replaced with strings!")
     return fnames
 
